eebf0aa54df788cd6f5f7c80dbc014a9807e487a
TANISHA BANSAL <tanishab130@gmail.com>
2024-09-29 11:43:30 +0530

    chatting

--- client/src/Components/ChatArea.js
+++ client/src/Components/ChatArea.js
@@ -4,51 +4,79 @@ import { IconButton } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import MessageSelf from "./MessageSelf";
 import MessageOthers from "./MessageOthers";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Skeleton from "@mui/material/Skeleton";
 import axios from "axios";
 import { myContext } from "./MainContainer";
+import { io } from "socket.io-client";
 
+// Initialize socket connection
+const socket = io("http://localhost:8080", {
+  transports: ["websocket"],
+  autoConnect: true,
+});
+
+/**
+ * ChatArea is a component that displays a chat between two users.
+ * It fetches messages when the component mounts and when the refresh state is updated.
+ * It also listens for new messages and updates the state when a new message is received.
+ * It takes in a chat_id and a user object as props.
+ * @param {string} chat_id The id of the chat to display.
+ * @param {object} user The user object of the current user.
+ * @returns {JSX.Element} The ChatArea component.
+ */
 function ChatArea() {
   const lightTheme = useSelector((state) => state.themeKey);
   const [messageContent, setMessageContent] = useState("");
   const messagesEndRef = useRef(null);
   const dyParams = useParams();
   const [chat_id, chat_user] = dyParams._id.split("&");
-  // console.log(chat_id, chat_user);
   const userData = JSON.parse(localStorage.getItem("userData"));
   const [allMessages, setAllMessages] = useState([]);
-  // console.log("Chat area id : ", chat_id._id);
-  // const refresh = useSelector((state) => state.refreshKey);
   const { refresh, setRefresh } = useContext(myContext);
-  const [loaded, setloaded] = useState(false);
-  const sendMessage = () => {
-    // console.log("SendMessage Fired to", chat_id._id);
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userData.data.token}`,
-      },
-    };
-    axios
-      .post(
-        "http://localhost:8080/message/",
-        {
-          content: messageContent,
-          chatId: chat_id,
-        },
-        config
-      )
-      .then(({ data }) => {
-        console.log("Message Fired");
-      });
-  };
-  // const scrollToBottom = () => {
-  //   messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  // };
+  const [loaded, setLoaded] = useState(false);
 
+  // Setup user when the component mounts
   useEffect(() => {
-    console.log("Users refreshed");
+    if (userData?.data?._id) {
+      socket.emit("setup", userData.data); // Send the user data to backend for setup
+    }
+
+    // Join chat room
+    socket.emit("join chat", chat_id);
+
+    return () => {
+      socket.off("setup");
+      socket.off("join chat");
+    };
+  }, [chat_id, userData.data]);
+
+  // Listen for new messages
+  useEffect(() => {
+    /**
+     * Updates the state with the new message, only if the new message is not already in the state.
+     * @param {object} newMessage The new message object.
+     */
+    const handleMessageReceived = (newMessage) => {
+      // Only update if the new message is not already in the state
+      setAllMessages((prevMessages) => {
+        if (!prevMessages.some((msg) => msg._id === newMessage._id)) {
+          return [...prevMessages, newMessage];
+        }
+        return prevMessages;
+      });
+    };
+
+    socket.on("message received", handleMessageReceived);
+
+    return () => {
+      socket.off("message received", handleMessageReceived);
+    };
+  }, []);
+
+  // Fetch messages when chat_id changes or refresh is triggered
+  useEffect(() => {
     const config = {
       headers: {
         Authorization: `Bearer ${userData.data.token}`,
@@ -57,12 +85,37 @@ function ChatArea() {
     axios
       .get("http://localhost:8080/message/" + chat_id, config)
       .then(({ data }) => {
+        // Only update state if there are new messages
         setAllMessages(data);
-        setloaded(true);
-        // console.log("Data from Acess Chat API ", data);
+        setLoaded(true);
       });
-    // scrollToBottom();
-  }, [refresh, chat_id, userData.data.token]);
+  }, [chat_id, refresh, userData.data.token]);
+
+  // Send a message to the server
+  const sendMessage = () => {
+    if (!messageContent) return; // Prevent sending empty messages
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userData.data.token}`,
+      },
+    };
+
+    const newMessage = {
+      content: messageContent,
+      chatId: chat_id,
+      sender: userData.data, // Include sender info
+    };
+
+    // Send the message to the server
+    axios
+      .post("http://localhost:8080/message/", newMessage, config)
+      .then(({ data }) => {
+        // Emit the new message to the backend after a successful send
+        socket.emit("new message", data);
+        setMessageContent(""); // Clear the input after sending
+      });
+  };
 
   if (!loaded) {
     return (
@@ -83,11 +136,7 @@ function ChatArea() {
         />
         <Skeleton
           variant="rectangular"
-          sx={{
-            width: "100%",
-            borderRadius: "10px",
-            flexGrow: "1",
-          }}
+          sx={{ width: "100%", borderRadius: "10px", flexGrow: "1" }}
         />
         <Skeleton
           variant="rectangular"
@@ -107,9 +156,6 @@ function ChatArea() {
             <p className={"con-title" + (lightTheme ? "" : " dark")}>
               {chat_user}
             </p>
-            {/* <p className={"con-timeStamp" + (lightTheme ? "" : " dark")}>
-              {props.timeStamp}
-            </p> */}
           </div>
           <IconButton className={"icon" + (lightTheme ? "" : " dark")}>
             <DeleteIcon />
@@ -123,10 +169,8 @@ function ChatArea() {
               const sender = message.sender;
               const self_id = userData.data._id;
               if (sender._id === self_id) {
-                // console.log("I sent it ");
                 return <MessageSelf props={message} key={index} />;
               } else {
-                // console.log("Someone Sent it");
                 return <MessageOthers props={message} key={index} />;
               }
             })}
@@ -137,24 +181,16 @@ function ChatArea() {
             placeholder="Type a Message"
             className={"search-box" + (lightTheme ? "" : " dark")}
             value={messageContent}
-            onChange={(e) => {
-              setMessageContent(e.target.value);
-            }}
+            onChange={(e) => setMessageContent(e.target.value)}
             onKeyDown={(event) => {
-              if (event.code == "Enter") {
-                // console.log(event);
+              if (event.code === "Enter") {
                 sendMessage();
-                setMessageContent("");
-                setRefresh(!refresh);
               }
             }}
           />
           <IconButton
             className={"icon" + (lightTheme ? "" : " dark")}
-            onClick={() => {
-              sendMessage();
-              setRefresh(!refresh);
-            }}
+            onClick={sendMessage}
           >
             <SendIcon />
           </IconButton>
